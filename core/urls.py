b3e1e2442e0dec5d3057b099aea4962a51deb6fa
Julian Gendritzki <jimbo.gend@icloud.com>
2026-03-31 11:57:42 +0200

    Added an sing in point and list all

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
+from .views import *
 
 
 urlpatterns = [
-
+    path('users/', UserList.as_view(), name='user-list'),
+    path('users/create/', UserCreate.as_view(), name='user-create'),
 ]
--- core/views.py
+++ core/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
-
+from rest_framework import permissions
+from rest_framework.generics import ListAPIView, CreateAPIView
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .models import *
+from .serializers import *
 # Create your views here.
+
+class UserList(ListAPIView):
+    permission_classes = [permissions.IsAuthenticated]
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+class UserCreate(CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
