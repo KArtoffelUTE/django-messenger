8a1ef90e4e9dfaad183d87b1332631573bfbde7d
KartoffelUTE <jimbo.gend@icloud.com>
2026-04-08 16:27:23 +0200

    Changed the System so that groups will be a thing soon

--- core/models.py
+++ core/models.py
@@ -3,12 +3,28 @@ from django.contrib.auth.models import User
 
 
 # Create your models here.
+class Conversation(models.Model):
+    typ = models.TextField()
+    name = models.TextField()
+    creator = models.ForeignKey(User, on_delete=models.CASCADE, related_name='conversations')
+    timestamp = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    def __str__(self):
+        return f'Conversation {self.typ} at {self.timestamp}'
+
 class Message(models.Model):
     sender = models.ForeignKey(User, on_delete=models.CASCADE, related_name='sent_messages')
-    recipient = models.ForeignKey(User, on_delete=models.CASCADE, related_name='received_messages')
+    conversation = models.ForeignKey(Conversation, on_delete=models.CASCADE, related_name='received_messages')
     content = models.TextField()
     timestamp = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return f'Message from {self.sender.username} to {self.recipient.username} at {self.timestamp}'
+        return f'Message from {self.sender.username} in Conversation {self.conversation.id} at {self.timestamp}'
+
+class ConversationParticipant(models.Model):
+    conversation = models.ForeignKey(Conversation, on_delete=models.CASCADE, related_name='participants')
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='conversations_participated')
+
+    def __str__(self):
+        return f'{self.user.username} in {self.conversation.name}'
